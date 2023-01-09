a902382badb309e523ffa40c408eb97a63cdec94
강나연 <seoultownroad@naver.com>
2023-01-09 10:07:56 +0900

    feat: pretendard 폰트 추가

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -6,8 +6,12 @@ const GlobalStyle = createGlobalStyle`
 
   html,
   body {
-    width: 100%;
-    height: 100%;
+    font-family: 'Pretendard Variable', Pretendard, -apple-system,
+      BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+      'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
+      'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+   
+    box-sizing: border-box;
   }
   
   #root {
