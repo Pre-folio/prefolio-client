95ba94053dd4011331993031d9d2a31b26eb9e7f
yjoonjang <yjoonjang34@gmail.com>
2023-01-09 13:29:15 +0900

    feat: 버튼, 태그 컴포넌트 제작

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import { Button } from '../components/elements/Button';
+import { Tag } from '../components/elements/Tag';
+
 function Home() {
-  return <div>랜딩 페이지</div>;
+  return (
+    <div>
+      랜딩 페이지
+      <Button type="small" color="mint" content="버튼" />
+      <Tag type="field" color="blue" content="개발" />
+    </div>
+  );
 }
 
 export default Home;
