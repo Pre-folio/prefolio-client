d81c091f2572bdd7e04f06c68a69881360285e61
강나연 <seoultownroad@naver.com>
2023-01-12 17:52:05 +0900

    feat: storybook 수정

--- src/components/common/Button/button.stories.tsx
+++ src/components/common/Button/button.stories.tsx
@@ -12,19 +12,23 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Big = Template.bind({});
 Big.args = {
   type: 'big',
+  color: 'mint',
 };
 
 export const Medium = Template.bind({});
 Medium.args = {
   type: 'medium',
+  color: 'mint',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   type: 'small',
+  color: 'mint',
 };
 
 export const Popup = Template.bind({});
 Popup.args = {
   type: 'popup',
+  color: 'mint',
 };
