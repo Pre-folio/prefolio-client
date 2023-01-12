459740a86c7b43249ee77d775cd8506a58f15ee1
yjoonjang <yjoonjang34@gmail.com>
2023-01-12 09:58:36 +0900

    feat: 프로필카드 컴포넌트 작업 완료 및 태그, 그림자 css 수정

--- src/components/common/Tag/index.tsx
+++ src/components/common/Tag/index.tsx
@@ -1,24 +1,33 @@
-import styled from 'styled-components';
+import styled, { CSSProperties } from 'styled-components';
 import { shadow, theme } from '../../../styles/theme';
 
 interface TagProps {
-  type: 'activity' | 'field';
-  color?: string;
+  type: 'activity' | 'field' | undefined;
+  sort?: 'plan' | 'dev' | 'design' | 'society' | 'intern' | 'project' | any;
   content?: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  isClicked?: boolean;
+  style?: CSSProperties;
 }
 
 interface ITag {
-  color?: 'mint' | 'blue' | 'purple' | any;
+  field: 'plan' | 'dev' | 'design' | any;
+  // color?: 'mint' | 'blue' | 'purple' | any;
 }
 
-export function Tag({ type, color, content, onClick }: TagProps) {
+/**
+ * 태그 컴포넌트
+ * @param type activity인지 field인지 - 둘다 해당 안되는 경우 공백
+ * @param sort society, intern, project, plan, dev, design, 이외에는 작성한대로
+ * @param style 인라인 style
+ */
+
+export function Tag({ type, sort, style }: TagProps) {
   return type === 'activity' ? (
-    <ActivityTag onClick={onClick}>{content || '동아리/학회'}</ActivityTag>
+    <ActivityTag style={style}>
+      {sort === 'society' ? '동아리/학회' : sort === 'intern' ? '인턴' : sort === 'project' ? '프로젝트' : sort}
+    </ActivityTag>
   ) : type === 'field' ? (
-    <FieldTag color={color} onClick={onClick}>
-      {content || '기획'}
+    <FieldTag field={sort} style={style}>
+      {sort === 'plan' ? '기획' : sort === 'dev' ? '개발' : sort === 'design' ? '디자인' : sort}
     </FieldTag>
   ) : (
     <></>
@@ -33,6 +42,8 @@ const ActivityTag = styled.button`
   background-color: ${theme.palette.Gray10};
   color: ${theme.palette.Gray50};
   box-shadow: ${shadow.Button.Black};
+
+  cursor: default;
 `;
 
 const FieldTag = styled.button<ITag>`
@@ -41,21 +52,23 @@ const FieldTag = styled.button<ITag>`
   height: 28px;
   border-radius: 4px;
   background-color: ${(props) =>
-    props.color === 'mint'
+    props.field === 'plan'
       ? theme.palette.Mint10
-      : props.color === 'blue'
+      : props.field === 'dev'
       ? theme.palette.Blue10
-      : props.color === 'purple' && theme.palette.Purple10};
+      : props.field === 'design' && theme.palette.Purple10};
   color: ${(props) =>
-    props.color === 'mint'
+    props.field === 'plan'
       ? theme.palette.Mint100
-      : props.color === 'blue'
+      : props.field === 'dev'
       ? theme.palette.Blue100
-      : props.color === 'purple' && theme.palette.Purple100};
+      : props.field === 'design' && theme.palette.Purple100};
   box-shadow: ${(props) =>
-    props.color === 'mint'
+    props.field === 'plan'
       ? shadow.Button.Green
-      : props.color === 'blue'
+      : props.field === 'dev'
       ? shadow.Button.Blue
-      : props.color === 'purple' && shadow.Button.Purple};
+      : props.field === 'design' && shadow.Button.Purple};
+
+  cursor: default;
 `;
