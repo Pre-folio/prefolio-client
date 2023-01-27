4b816c55ca6371bb7070581c078201bf1c901321
강나연 <seoultownroad@naver.com>
2023-01-28 04:32:31 +0900

    feat :q : TagTabBar 아이콘 적용

--- src/components/feed/TagTabBar.tsx
+++ src/components/feed/TagTabBar.tsx
@@ -1,6 +1,9 @@
+import { useEffect } from 'react';
 import { Dispatch, SetStateAction, useRef } from 'react';
 import styled, { css } from 'styled-components';
+import { Dot } from '../../assets/icons';
 import { theme } from '../../styles/theme';
+import { Text } from '../common/Wrapper';
 
 export interface TabBarProps {
   barState: boolean;
@@ -22,46 +25,55 @@ export interface TabBarProps {
 export const TagTabBar = ({
   barState,
   setBarState,
-  leftTab = '・ 최신순',
-  rightTab = '・ 추천순',
+  leftTab = '최신순',
+  rightTab = '추천순',
 }: TabBarProps) => {
   const ref = useRef<HTMLDivElement[] | null[]>([]);
 
   const HandleTabClick = (e: any) => {
-    setBarState(e.target === ref.current[0]?.childNodes[0]);
+    setBarState(e.target.parentNode.id === 'new' ? true : false);
   };
 
   return (
     <TabBarWrapper>
-      <div ref={(el) => (ref.current[0] = el)} onClick={HandleTabClick}>
-        <Tab barState={barState}>{leftTab}</Tab>
-      </div>
-      <div ref={(el) => (ref.current[1] = el)} onClick={HandleTabClick}>
-        <Tab barState={!barState}>{rightTab}</Tab>
-      </div>
+      <Button id='new' onClick={HandleTabClick} barState={barState}>
+        <Dot
+          fill={barState ? `${theme.palette.Black}` : `${theme.palette.Gray20}`}
+        />
+        <Text typo='Body2' color={barState ? 'Black' : 'Gray20'} height={16}>
+          {leftTab}
+        </Text>
+      </Button>
+      <Button id='like' onClick={HandleTabClick} barState={barState}>
+        <Dot
+          fill={barState ? `${theme.palette.Gray20}` : `${theme.palette.Black}`}
+        />
+        <Text typo='Body2' color={barState ? 'Gray20' : 'Black'} height={16}>
+          {rightTab}
+        </Text>
+      </Button>
     </TabBarWrapper>
   );
 };
 
 const TabBarWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+
   cursor: pointer;
+
+  width: 128px;
 `;
 
-const Tab = styled.div<{
+const Button = styled.button<{
   barState: boolean;
 }>`
-  width: 64px;
+  width: 58px;
   height: 16px;
-
-  ${({ barState }) =>
-    css`
-      color: ${barState ? `${theme.palette.Black}` : `${theme.palette.Gray20}`};
-    `}
-
-  ${theme.typo.Body2}
+  padding: 0;
 
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  gap: 0;
 `;
