83ad4f4091de19e3c75afafb6243148f13ad33a4
강나연 <seoultownroad@naver.com>
2023-01-28 16:18:08 +0900

    feat: useFeed 훅에 getSearch 추가

--- src/components/search/SearchPosts.tsx
+++ src/components/search/SearchPosts.tsx
@@ -1,3 +1,40 @@
-export const SearchPosts = () => {
+import { useEffect, useState } from 'react';
+import { useFeed } from '../../hooks/useFeed';
+import { Posts } from '../feed/Posts';
+
+export const SearchPosts = (props: any) => {
+  const {
+    posts,
+    setPosts,
+    getFeed,
+    getSearch,
+    nowRead,
+    searched,
+    setSearched,
+  } = useFeed();
+
+  useEffect(() => {
+    if (props.values) {
+      getSearch(props.values);
+      setSearched(true);
+    } else {
+      setSearched(false);
+    }
+    console.log('hi', props.value);
+  }, [props.value]);
+
+  if (!searched) {
+    return (
+      <div>
+        <Posts posts={nowRead} />
+      </div>
+    );
+  } else if (searched) {
+    return (
+      <div>
+        <Posts posts={posts} />
+      </div>
+    );
+  }
   return <div></div>;
 };
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,18 +1,24 @@
+import { useInput } from '@mui/base';
 import { SearchBox } from '../../components/common/SearchBox';
 import { Line, Space, Text } from '../../components/common/Wrapper';
+import { SearchPosts } from '../../components/search/SearchPosts';
 import { theme } from '../../styles/theme';
 
 const Search = () => {
+  const input = useInput;
+
   return (
-    <div>
+    <div style={{ height: '130vh' }}>
       <Space height={106} />
-      <SearchBox />
+      <SearchBox {...input} />
       <Space height={60} />
       <Line width={1200} height={1} />
       <Space height={60} />
       <Text typo='Heading3' color='Black' height={24}>
         현재 많은 프리폴리오 유저들이 읽고 있어요
       </Text>
+      <Space height={60} />
+      <SearchPosts {...input} />
     </div>
   );
 };
