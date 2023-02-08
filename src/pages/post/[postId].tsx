6352390369f228fe4339f9ccda70b6d925cfab52
yjoonjang <yjoonjang34@gmail.com>
2023-02-09 03:25:30 +0900

    Merge

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -14,6 +14,7 @@ import ProfileArea from '../../components/postPage/ProfileArea';
 import { IUserInfo } from '../../interfaces';
 import { theme } from '../../styles/theme';
 import { userState } from '../../store/Auth/userState';
+import { GetServerSideProps } from 'next';
 
 const Board = (props: any) => {
   const Viewer = dynamic(() => import('../../components/postPage/TextViewer'), {
@@ -191,7 +192,7 @@ const Board = (props: any) => {
 
 export default Board;
 
-export async function getServerSideProps(context: any) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const { postId }: any = context.query;
   const postIdToNumber = Number(postId);
   const queryClient = new QueryClient();
@@ -203,7 +204,7 @@ export async function getServerSideProps(context: any) {
       postId: postIdToNumber,
     },
   };
-}
+};
 
 const FloatingButtonWrapper = styled.div`
   position: fixed;
