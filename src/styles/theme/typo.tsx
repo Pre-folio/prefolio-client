1159d0135e4958c58b83f422008651bcc88ed696
kongnayeon <seoultownroad@naver.com>
2023-07-19 14:48:51 +0900

    refactor: 타이포 변경 반영

--- src/styles/theme/typo.tsx
+++ src/styles/theme/typo.tsx
@@ -3,74 +3,99 @@ import { css } from 'styled-components';
 export const calcRem = (px: number) => `${px / 16}rem`;
 
 export const typo = {
-  Heading1: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 700;
-    font-size: ${calcRem(40)};
-    line-height: 150%;
-  `,
-  Heading2: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 600;
-    font-size: ${calcRem(32)};
-    line-height: 162.5%;
-  `,
-  Heading3: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 600;
-    font-size: ${calcRem(24)};
-    line-height: 183.3%;
-  `,
-  Heading4: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 700;
-    font-size: ${calcRem(22)};
-    line-height: 136.5%;
-  `,
-  Heading5: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 500;
-    font-size: ${calcRem(22)};
-    line-height: 172.72%;
-  `,
-  Body1: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 500;
-    font-size: ${calcRem(18)};
-    line-height: 166.66%;
-  `,
-  Body2: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 400;
-    font-size: ${calcRem(16)};
-    line-height: 162.5%;
-  `,
-  Label1: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 600;
-    font-size: ${calcRem(18)};
-    line-height: 100%;
-  `,
-  Label2: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 500;
-    font-size: ${calcRem(16)};
-    line-height: 100%;
-  `,
-  Gotham: css`
-    font-family: 'Gotham';
-    font-style: normal;
-    font-weight: 500;
-    font-size: ${calcRem(64)};
-    line-height: 131.5%;
+    Heading1: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 700;
+        font-size: ${calcRem(40)};
+        line-height: 150%;
+    `,
+    Heading2: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 600;
+        font-size: ${calcRem(32)};
+        line-height: 130%;
+    `,
+    Heading3: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 600;
+        font-size: ${calcRem(20)};
+        line-height: 100%;
+    `,
+    Heading3_1: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 600;
+        font-size: ${calcRem(20)};
+        line-height: 140%;
+    `,
+    Heading4: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 700;
+        font-size: ${calcRem(16)};
+        line-height: 100%;
+        letter-spacing: ${calcRem(8)};
+    `,
+    Heading5: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 500;
+        font-size: ${calcRem(16)};
+        line-height: 130%;
+    `,
+    HeadingMobile: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 500;
+        font-size: ${calcRem(22)};
+        line-height: 100%;
+    `,
+    Body1: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 500;
+        font-size: ${calcRem(18)};
+        line-height: 160%;
+    `,
+    Body2: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 400;
+        font-size: ${calcRem(14)};
+        line-height: 160%;
+    `,
+    Body3: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 400;
+        font-size: ${calcRem(12)};
+        line-height: 100%;
+    `,
+    Label1: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 600;
+        font-size: ${calcRem(14)};
+        line-height: 100%;
+    `,
+    Label1_1: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 700;
+        font-size: ${calcRem(14)};
+        line-height: 100%;
+    `,
+    Label2: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 700;
+        font-size: ${calcRem(12)};
+        line-height: 100%;
+    `,
+    Gotham: css`
+        font-family: 'Gotham';
+        font-style: normal;
+        font-weight: 500;
+        font-size: ${calcRem(64)};
+        line-height: 131.5%;
 
-    letter-spacing: -0.05em;
-    text-transform: capitalize;
-  `,
-  Landing: css`
-    font-family: 'Pretendard', 'Apple SD Gothic Neo';
-    font-weight: 600;
-    font-size: ${calcRem(60)};
-    line-height: 150%;
-  `,
+        letter-spacing: -0.05em;
+        text-transform: capitalize;
+    `,
+    Landing: css`
+        font-family: 'Pretendard', 'Apple SD Gothic Neo';
+        font-weight: 600;
+        font-size: ${calcRem(60)};
+        line-height: 150%;
+    `,
 } as const;
